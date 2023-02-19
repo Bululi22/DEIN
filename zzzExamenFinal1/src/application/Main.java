a37ed794475973158c837f6cab85028e24742d38
Adrián <adrian.ferrad@gmail.com>
2023-02-19 23:41:29 +0100

    Animo

---
+++ zzzExamenFinal1/src/application/Main.java
@@ -0,0 +1,53 @@
+package application;
+	
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import conexionBD.Propiedades;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			//Idioma
+//			String idioma = Propiedades.getValor("idioma");
+//			String region = Propiedades.getValor("region");
+//			Locale.setDefault(new Locale(idioma, region));
+//			ResourceBundle bundle = ResourceBundle.getBundle("idiomas/messages");
+			
+			//Rutas (SI METES IDIOMA METE AL FINAL ",bundle")
+			GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("/fxml/Sample.fxml"));
+			Scene scene = new Scene(root,998,525);
+			scene.getStylesheets().add(getClass().getResource("/css/application.css").toExternalForm());
+			
+			//Icono
+//			String img=getClass().getResource("/images/agenda.png").toString();
+//			primaryStage.getIcons().add(new Image(img));
+			
+			//Mostrar
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+			//Redimensionable
+//			primaryStage.setResizable(false);
+//			primaryStage.setMaximized(false);
+			
+			//Titulo
+			primaryStage.setTitle("AGENDA");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ zzzExamenFinal1/src/module-info.java
@@ -0,0 +1,13 @@
+module ExamenFinal1 {
+	requires javafx.controls;
+	requires java.desktop;
+	requires javafx.web;
+	requires javafx.fxml;
+	requires javafx.swing;
+	requires javafx.media;
+	requires javafx.graphics;
+	requires javafx.base;
+	requires java.sql;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
